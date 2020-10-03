7302aa62b385917ca53eb165561245869e666587
vinny <vinny@kraken>
2020-10-03 08:11:35 +0530

    added a new systray patch

--- config.def.h
+++ config.def.h
@@ -4,6 +4,10 @@
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int gappx     = 5;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
+static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
+static const unsigned int systrayspacing = 2;   /* systray spacing */
+static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
+static const int showsystray        = 1;     /* 0 means no systray */
 static const int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
@@ -69,9 +73,9 @@ static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
-	{ MODKEY,						XK_w,	   spawn,		   {.v = wifi_scan } },
-	{ MODKEY|ShiftMask,				XK_w,	   spawn,		   {.v = wifi_disc } },
-	{ MODKEY,						XK_q,	   spawn,		   {.v = exit_wm } },
+	{ MODKEY,			XK_w,	   spawn,	   {.v = wifi_scan } },
+	{ MODKEY|ShiftMask,		XK_w,	   spawn,	   {.v = wifi_disc } },
+	{ MODKEY,			XK_q,	   spawn,	   {.v = exit_wm } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
